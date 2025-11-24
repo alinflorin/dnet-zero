828e09377bd02d3da56f92c1dff2d9df41e09248
Alin-Florin Ciu <alinflorinciu@icloud.com>
2025-11-24 22:43:48 +0200

    asd

--- Program.cs
+++ Program.cs
@@ -4,6 +4,5 @@ using dnet_zero_engine;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
 
 await builder.Build().RunAsync();
